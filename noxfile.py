f82570db444692754ab900e58b95b1c85f1fa1a2
Ignacio Maiz <ignacio.maiz@gmail.com>
2023-07-19 15:26:42 +0100

    added `nox` session for deploying docs

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@
 import json
 import os
 import pathlib
-from typing import Dict, List
+from typing import Dict, List, cast
 
 import nox
 
@@ -63,7 +63,7 @@ def tests_e2e_nocov(session: nox.Session) -> None:
 @nox.session(python=python_versions, venv_backend="mamba", reuse_venv=True)
 def tests_mamba(session: nox.Session) -> None:
     """Run tests locally with `mamba` as the backend."""
-    session.conda_install("--file", "requirements.txt")
+    session.conda_install("--file", "requirements.txt", channel="conda-forge")
     session.install("-e", ".[test]")
 
     session.run(
@@ -82,7 +82,7 @@ def tests_mamba(session: nox.Session) -> None:
 @nox.session(python=python_versions, venv_backend="mamba", reuse_venv=True)
 def tests_mamba_nocov(session: nox.Session) -> None:
     """Run tests locally with `mamba` as the backend with no coverage."""
-    session.conda_install("--file", "requirements.txt")
+    session.conda_install("--file", "requirements.txt", channel="conda-forge")
     session.install("-e", ".[test]")
 
     session.run("pytest", "-m", "not e2e", *session.posargs, external=True)
@@ -91,7 +91,7 @@ def tests_mamba_nocov(session: nox.Session) -> None:
 @nox.session(python="3.11", venv_backend="mamba", reuse_venv=True)
 def tests_mamba_e2e(session: nox.Session) -> None:
     """Run end to end tests locally with `mamba` as the backend."""
-    session.conda_install("--file", "requirements.txt")
+    session.conda_install("--file", "requirements.txt", channel="conda-forge")
     session.install("-e", ".[test]")
 
     session.run(
@@ -110,7 +110,7 @@ def tests_mamba_e2e(session: nox.Session) -> None:
 @nox.session(python="3.11", venv_backend="mamba", reuse_venv=True)
 def tests_mamba_e2e_nocov(session: nox.Session) -> None:
     """Run end to end tests locally with `mamba` as the backend with no coverage."""
-    session.conda_install("--file", "requirements.txt")
+    session.conda_install("--file", "requirements.txt", channel="conda-forge")
     session.install("-e", ".[test]")
 
     session.run("pytest", "-m", "e2e", *session.posargs, external=True)
@@ -146,11 +146,13 @@ def coverage(session: nox.Session) -> None:
 @nox.session(python="3.11")
 def lint(session: nox.Session) -> None:
     """Lint package."""
-    session.install("pylint", "mypy")
-    session.install("-e", ".")
+    session.install("-e", ".[lint]")
 
-    session.run("pylint", "bavapi")
     session.run("mypy", "bavapi")
+    session.run("pylint", "bavapi")
+    session.run("pylint", "tests")
+    session.run("isort", "-l", "100", ".")
+    session.run("black", ".")
 
 
 @nox.session(python=None)
@@ -192,9 +194,77 @@ def build(session: nox.Session) -> None:
     session.run("python", "-m", "build")
 
 
-@nox.session(python=None)
-def publish(session: nox.Session) -> None:
-    """Publish package wheel and source distribution to PyPI."""
-    session.install("twine")
+@nox.session(python=False)
+def docs_deploy(session: nox.Session) -> None:
+    """Build and deploy documentation with version handling.
 
-    session.run("twine", "upload", "dist/*")
+    If package minor version is latest, deploy docs version and assign to `latest`.
+    If package minor version is not latest, deploy docs version.
+    If package minor version exists, update docs version.
+
+    Arguments
+    ---------
+    --push: flag
+        Check versions from GitHub branch and deploy to branch.
+    --version: str
+        Override version from package
+
+    Examples
+    --------
+    Run docs_deploy locally
+
+    `nox -s docs_deploy`
+
+    Run docs_deploy for remote branch
+
+    `nox -s docs_deploy -- --push`
+
+    Override package version
+
+    `nox -s docs_deploy -- --version=2.0.0`
+    """
+
+    # Get current package version
+
+    try:
+        import tomllib
+    except ImportError:
+        session.install("tomli")
+        import tomli as tomllib
+
+    def version_tuple(string: str) -> tuple[int, ...]:
+        return tuple(int(i) for i in string.split("."))
+
+    if not any(
+        version_args := [i for i in session.posargs if i.startswith("--version=")]
+    ):
+        with open("pyproject.toml", "rb") as file:
+            version = version_tuple(tomllib.load(file)["project"]["version"])
+    else:
+        version = version_tuple(version_args[0].rpartition("=")[-1])
+
+    minor_str = ".".join(str(i) for i in version[:2])
+
+    remote = "--push" in session.posargs
+    remote_str = "remotely " if remote else ""
+    list_args = ("-b", "gh-pages") if remote else ()
+    deploy_args = ["--push", minor_str] if remote else [minor_str]
+
+    # Get deployed versions from branch
+    out = cast(str, session.run("mike", "list", *list_args, silent=True))
+    versions = [
+        tuple(int(i) for i in v.partition(" ")[0].split(".")) for v in out.splitlines()
+    ]
+
+    if version[:2] in versions:
+        print(f"Updating docs {remote_str}for version {minor_str}...")
+
+    elif all(version[:2] > v for v in versions):
+        print(f"Deploying docs {remote_str}for version {minor_str} as latest...")
+        deploy_args.extend(("--update-aliases", "latest"))
+    else:
+        print(f"Deploying docs {remote_str}for version {minor_str} as latest...")
+        deploy_args.append("latest")
+
+    print(f"Calling deploy with args: {deploy_args}")
+    # session.run("mike", "deploy", *deploy_args)
