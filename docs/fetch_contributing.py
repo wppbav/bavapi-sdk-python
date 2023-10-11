e79d829eb8eec556266e44ecbcb129c0d16174f1
Ignacio Maiz <ignacio.maiz@gmail.com>
2023-10-11 17:37:09 +0200

    added relative documentation link support

--- docs/fetch_contributing.py
+++ docs/fetch_contributing.py
@@ -1,11 +1,25 @@
-with open("./CONTRIBUTING.md", encoding="utf-8") as file:
-    contributing = file.read()
+import re
 
-mkdocs_header = """---
+# Replace absolute documentation URLs with relative URIs
+# <rel> capture group finds relative path
+# <section> capture group finds section index (including "#")
+DOCS_URL_PAT = (
+    r"\(https://wppbav\.github\.io/bavapi-sdk-python/latest/"
+    r"(?P<rel>[^#\)]+)(?P<section>[^\)]*)\)"
+)
+
+MKDOCS_HEADER = """---
 hide:
     - navigation
 ---
 """
 
+with open("./CONTRIBUTING.md", encoding="utf-8") as file:
+    contributing = file.read()
+
+# Substitute found docs links with relative docs path
+# Add ".md" to final relative URL before section index
+contributing = re.sub(DOCS_URL_PAT, r"(\g<rel>.md\g<section>)", contributing)
+
 with open("./docs/contributing.md", "w", encoding="utf-8") as file:
-    file.write("\n".join((mkdocs_header, contributing)))
+    file.write("\n".join((MKDOCS_HEADER, contributing)))
