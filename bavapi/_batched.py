24e3252fb8791753dfa4c8865516fc62f033d5e9
Ignacio Maiz <ignacio.maiz@gmail.com>
2023-11-30 20:45:47 +0000

    use itertools.batched on py3.12+

--- bavapi/_batched.py
+++ bavapi/_batched.py
@@ -1,14 +1,21 @@
+# pylint: disable=invalid-name
+
 import itertools
+import sys
 from typing import Iterable, Iterator, Tuple, TypeVar
 
 T = TypeVar("T")
 
+if sys.version_info < (3, 12):
 
-def batched(iterable: Iterable[T], n: int) -> Iterator[Tuple[T, ...]]:
-    "Batch data into tuples of length n. The last batch may be shorter."
-    # batched('ABCDEFG', 3) --> ABC DEF G
-    if n < 1:
-        raise ValueError("n must be at least one")
-    it = iter(iterable)
-    while batch := tuple(itertools.islice(it, n)):
-        yield batch
+    def batched(iterable: Iterable[T], n: int) -> Iterator[Tuple[T, ...]]:
+        "Batch data into tuples of length n. The last batch may be shorter."
+        # batched('ABCDEFG', 3) --> ABC DEF G
+        if n < 1:
+            raise ValueError("n must be at least one")
+        it = iter(iterable)
+        while batch := tuple(itertools.islice(it, n)):
+            yield batch
+
+else:
+    batched = itertools.batched
