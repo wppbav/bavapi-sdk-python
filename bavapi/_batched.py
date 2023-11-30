22d02ae98c83e0499133e37561375123c511554f
Ignacio Maiz <ignacio.maiz@gmail.com>
2023-11-30 22:23:09 +0000

    refactored pre-py3.12 batched into class

--- bavapi/_batched.py
+++ bavapi/_batched.py
@@ -2,20 +2,23 @@
 
 import itertools
 import sys
-from typing import Iterable, Iterator, Tuple, TypeVar
+from typing import Generic, Iterable, Iterator, Tuple, TypeVar
 
 T = TypeVar("T")
 
 if sys.version_info < (3, 12):
 
-    def batched(iterable: Iterable[T], n: int) -> Iterator[Tuple[T, ...]]:
-        "Batch data into tuples of length n. The last batch may be shorter."
-        # batched('ABCDEFG', 3) --> ABC DEF G
-        if n < 1:
-            raise ValueError("n must be at least one")
-        it = iter(iterable)
-        while batch := tuple(itertools.islice(it, n)):
-            yield batch
+    class batched(Generic[T]):
+        def __init__(self, iterable: Iterable[T], n: int) -> None:
+            if n < 1:
+                raise ValueError("n must be at least one")
+            self.iterable = iterable
+            self.n = n
+
+        def __iter__(self) -> Iterator[Tuple[T, ...]]:
+            it = iter(self.iterable)
+            while batch := tuple(itertools.islice(it, self.n)):
+                yield batch
 
 else:
     batched = itertools.batched
